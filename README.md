d25870ae6b9a0488168b2c79204f891850aa60b1
SarumathyPrabakaran <harinisarumathi@gmail.com>
2023-05-16 19:41:38 +0530

    yes.. working

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # todo-app
 todo-app
+A simple todo app to display tasks. It uses postgreSQL database to store and retreive data.
+Note that env file is set to configure postgres db.
--- app.py
+++ app.py
@@ -1,13 +1,25 @@
+# Import necessary modules
 from flask import Flask, render_template,  request
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+load_dotenv() 
 
+database   = os.getenv('DB_NAME')
+user       =os.getenv('DB_USER')
+password   =os.getenv('DB_PASS')
+host       =os.getenv('DB_HOST')
+port       = (os.getenv('DB_PORT'))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test@0.0.0.0:5432/saru'
+
+# Configure the PostgreSQL database connection
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}:{port}/{database}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
+# Create a class representing the database schema for the todo table
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
@@ -19,24 +31,32 @@ class Todo(db.Model):
     def __init__(self, id, title):
         self.id = id
         self.title = title
-    
-objs = []
+   
+   
+
+# Set up a route for rendering the HTML template and processing POST requests
 @app.route('/',methods = ['GET','POST'])
 def index():
-    global objs
+    
+    # Fetch all todo items from the database
     objs = db.session.query(Todo).all() 
+    
+    # Handle form submission
     if request.method == "POST":
         now = request.form.get("todo")
         
-        
-        now_id = db.session.query(Todo).all()
-        
+        # Create a new Todo object with the submitted title
+        now_id = db.session.query(Todo.id).all()
         todo_obj =  Todo(len(now_id)+1,now)
+        
+        # Add the new todo to the database session and commit the changes
         db.session.add(todo_obj)
         db.session.commit()
+        
+        # Refresh the list of todos from the database
         objs = db.session.query(Todo).all() 
-        return render_template('index.html', l = objs)
- 
+        
+      # Render the index.html template and pass the list of todos to it
     return render_template('index.html', l = objs)
 
 if __name__ == "__main__":
